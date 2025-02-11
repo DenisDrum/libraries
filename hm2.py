e1ab73362485d87cbea112d1bd669a8a15ca6fac
Denis <8z2monastyrmyy@gmail.com>
2025-02-11 17:03:35 +0300

    дз 2

---
+++ hm2.py
@@ -0,0 +1,18 @@
+import numpy as np
+#1
+# a = np.ones((3,2))
+# b = np.arange(3)[:,np.newaxis]
+# print(b)
+# print(b.shape)
+#было
+#2 (3,2) -> (3,2) -> (3,2)
+#1 (3, ) -> (1,3) -> (3,3)
+#стало
+#2 (3,2) -> (3,2) 
+#1 (3,1) -> (3,2)
+# c = a + b
+# print(c,c.shape)
+#2
+y = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(np.sum((y > 3) & (y < 9)))
+
